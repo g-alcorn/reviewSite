8ba2382936198851f02369e7f06937beed431a69
Griffin Alcorn <griffin.alcorn@gmail.com>
2021-06-22 23:58:11 -0400

    components started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Nav, Button } from "react-bootstrap/Nav";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AddReview from "./components/addReview";
+import Restaurant from "./components/restaurant";
+import RestaurantsList from "./components/restaurantsList";
+import Login from "./components/login";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  };
+
+  async function logout() {
+    setUser(null);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Nav.Item>
+          <Nav.Link href="/restaurants">Restaurant Reviews</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          { user ? (
+            <Nav.Link href="/logout" onClick={logout}>Logout {user.name}</Nav.Link>
+          ) : (
+            <Nav.Link href="/login">Login</Nav.Link>
+          )}
+        </Nav.Item>
+      </Nav>
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/addReview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddReview() {
+  return (
+    <div className="addReview">
+
+    </div>
+  );
+}
+
+export default AddReview;
