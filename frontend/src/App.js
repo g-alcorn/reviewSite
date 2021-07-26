2a02ed8f54e22f2dec899fbc1a52b21ccc74d8c7
Griffin Alcorn <griffin.alcorn@gmail.com>
2021-07-25 20:31:04 -0400

    Navbar fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import { Nav, Button } from "react-bootstrap/Nav";
+import { Nav, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AddReview from "./components/addReview";
@@ -18,24 +18,62 @@ function App() {
   async function logout() {
     setUser(null);
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
 
-      <Nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Nav.Item>
-          <Nav.Link href="/restaurants">Restaurant Reviews</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          { user ? (
-            <Nav.Link href="/logout" onClick={logout}>Logout {user.name}</Nav.Link>
-          ) : (
-            <Nav.Link href="/login">Login</Nav.Link>
-          )}
-        </Nav.Item>
-      </Nav>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">Restaurants</Link>
+          </li>
+          <li className="nav-item" >
+            { user ? (
+              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                Logout {user.name}
+              </a>
+            ) : (            
+            <Link to={"/login"} className="nav-link">Login</Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div>
+        <Switch>
+          <Route 
+            exact 
+            path={["/", "/restaurants"]} 
+            component={RestaurantsList} 
+          />
+
+          <Route
+            path="/restaurants/:id/review"
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )}
+          />
+
+          <Route
+            path="/restaurants/:id"
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )}
+          />
+
+          <Route 
+            path="/login"
+            render={(props) => (
+              <Login {...props} login={login} />
+            )}
+          />
+        </Switch>
+      </div>
     </div>
   );
 };
